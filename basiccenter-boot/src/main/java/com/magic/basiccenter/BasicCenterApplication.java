7dd0f06b33bf292e9476460b445c64db1979689c
zhuzh <wlvyh15820yh>
2020-08-18 15:54:27 +0800

    init project

---
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
@@ -0,0 +1,28 @@
+package com.magic.basiccenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * <p>基础中心-启动类</P>
+ *
+ * @author zhuzh@belink.com
+ * @version 0.0.1
+ * @className basicCenterApplication
+ * @sine 2020/8/17 9:15
+ */
+@SpringBootApplication
+@ImportResource(locations = {"classpath*:rpc-provider.xml", "classpath*:rpc-consumer.xml"})
+@ComponentScan(basePackages = {"com.gift", "com.magic.basiccenter"})
+@MapperScan("com.gift.**.**.model.mapper")
+@EnableDiscoveryClient
+public class BasicCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicCenterApplication.class,args);
+    }
+}
---
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+//package com.magic.basiccenter.config;
+//
+//import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//
+///**
+// * @description: MybatisPlus分页插件配置
+// * @author: yangquan
+// * @create: 2020-03-16
+// */
+////@Configuration
+////@Component
+//public class MybatisPlusConfig {
+//    /**
+//     * 分页插件
+//     */
+//    @Bean
+//    @Qualifier("RolePaginationInterceptor")
+//    public PaginationInterceptor paginationInterceptor() {
+//        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+//        //最大条数
+//        paginationInterceptor.setLimit(100);
+//        return paginationInterceptor;
+//    }
+//}
