7a7c8e09591e5172fb6cb7b66aedc4f916036e09
advert <351687008@qq.com>
2020-08-19 18:10:21 +0800

    广告项目1

--- basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
@@ -18,7 +18,8 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource(locations = {"classpath*:rpc-provider.xml", "classpath*:rpc-consumer.xml"})
 @ComponentScan(basePackages = {"com.gift", "com.magic.basiccenter"})
-@MapperScan("com.gift.**.**.model.mapper")
+//@MapperScan("com.gift.**.**.model.mapper")
+@MapperScan("com.magic.basiccenter.model.mapper")
 @EnableDiscoveryClient
 public class BasicCenterApplication {
 
--- basiccenter-boot/src/main/java/com/magic/basiccenter/controller/BasicController.java
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/controller/BasicController.java
@@ -1,5 +1,19 @@
 package com.magic.basiccenter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.magic.application.infrastructure.service.dto.MagicDTO;
+import com.magic.application.infrastructure.service.dto.MagicOutDTO;
+import com.magic.basiccenter.dto.AdvertSelDTO;
+import com.magic.basiccenter.dto.AdvertSelOutPageDTO;
+import com.magic.basiccenter.service.IAdvertSelCond;
+
+
 /**
  * <p>基础中心--控制器</P>
  *
@@ -8,5 +22,21 @@ package com.magic.basiccenter.controller;
  * @className basicController
  * @sine 2020/8/17 9:13
  */
+@RestController
+@RequestMapping("advert")
 public class BasicController {
+	
+	@Autowired
+	private IAdvertSelCond advertSelCond;
+	
+	@PostMapping("advertSelCond")
+	public MagicOutDTO<AdvertSelOutPageDTO> advertSelCond(@RequestBody MagicDTO<AdvertSelDTO> requestDTO){
+		return advertSelCond.advertSelCond(requestDTO);
+	}
+	
+	@GetMapping("test")
+	public String testC(String hello) {
+		return hello;
+	}
+	
 }
