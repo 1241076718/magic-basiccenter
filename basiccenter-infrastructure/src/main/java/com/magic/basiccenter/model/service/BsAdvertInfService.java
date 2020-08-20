ef74dbad4979a0d0e020199a885d50523dba5aac
laiqx <842442235@qq.com>
2020-08-20 11:09:34 +0800

    移除废弃的文件

--- basiccenter-infrastructure/src/main/java/com/magic/basiccenter/model/service/BsAdvertInfService.java
+++
@@ -1,8 +0,0 @@
-package com.magic.basiccenter.model.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.magic.basiccenter.model.entity.BsAdvertInf;
-
-public interface BsAdvertInfService extends IService<BsAdvertInf>{
-
-}
