81a72294bdd092377020c91d6e71b54e5f033da1
zhuzh <wlvyh15820yh>
2020-08-21 10:17:23 +0800

    projct all commit by bug

--- basiccenter-boot/src/main/java/com/magic/basiccenter/controller/BasicController.java
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/controller/BasicController.java
@@ -1,5 +1,11 @@
 package com.magic.basiccenter.controller;
 
+import com.magic.application.infrastructure.service.dto.MagicDTO;
+import com.magic.basiccenter.BasicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * <p>基础中心--控制器</P>
  *
@@ -8,5 +14,15 @@ package com.magic.basiccenter.controller;
  * @className basicController
  * @sine 2020/8/17 9:13
  */
+@RestController
 public class BasicController {
+
+    @Autowired
+    BasicService basicService;
+
+
+    @GetMapping("delete")
+    public MagicDTO delete(){
+        return basicService.delete();
+    }
 }
