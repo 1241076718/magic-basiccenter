85f05111b92b5d8b522969bb098d58c4ad6b4b64
zhuzh <wlvyh15820yh>
2020-08-21 09:58:49 +0800

    basicApplication change

--- basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
@@ -3,9 +3,11 @@ package com.magic.basiccenter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.DebuggingClassWriter;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * <p>基础中心-启动类</P>
@@ -18,11 +20,13 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource(locations = {"classpath*:rpc-provider.xml", "classpath*:rpc-consumer.xml"})
 @ComponentScan(basePackages = {"com.gift", "com.magic.basiccenter"})
-@MapperScan("com.gift.**.**.model.mapper")
+@MapperScan("com.**.model.mapper")
 @EnableDiscoveryClient
+@EnableTransactionManagement
 public class BasicCenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasicCenterApplication.class,args);
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\class");
+//        SpringApplication.run(BasicCenterApplication.class,args);
     }
 }
