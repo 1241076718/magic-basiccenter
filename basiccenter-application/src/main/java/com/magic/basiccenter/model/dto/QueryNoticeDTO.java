57ba812ac6b86ebda596a23ad879114b7ea4e73a
liubing <1241076718@qq.com>
2020-08-21 09:45:53 +0800

    修改错误1 08/21 10:00

--- basiccenter-application/src/main/java/com/magic/basiccenter/model/dto/QueryNoticeDTO.java
+++ basiccenter-application/src/main/java/com/magic/basiccenter/model/dto/QueryNoticeDTO.java
@@ -30,9 +30,9 @@ public class QueryNoticeDTO implements Serializable {
 
 	private String niNtcText;
 
-	private String niNtcStartTime;
+	private Date niNtcStartTime;
 
-	private String niNtcEndTime;
+	private Date niNtcEndTime;
 
 	private Integer niNtcCount;
 
@@ -47,7 +47,6 @@ public class QueryNoticeDTO implements Serializable {
 			timezone = "GMT+8"
 	)
 	private Date niNtcGmtModified;
-	private java.sql.Timestamp niNtcGmtCreate;
 	private String niNtcGmtModifier;
 	private Integer nowsPage;//当前页数
 	private Integer pageSize;//页面显示条数
--- basiccenter-infrastructure/src/main/java/com/magic/basiccenter/service/impl/UpdateNoticeServiceDaoImpl.java
+++ basiccenter-infrastructure/src/main/java/com/magic/basiccenter/service/impl/UpdateNoticeServiceDaoImpl.java
@@ -4,56 +4,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gift.core.utils.SpringContextUtils;
-import com.magic.basiccenter.model.dto.UpdateNoticeDTO;
-import com.magic.basiccenter.model.dto.UpdateNoticeOutDTO;
-import com.magic.basiccenter.model.entity.Notice;
-import com.magic.basiccenter.model.mapper.UpdateNoticeMapper;
-import com.magic.basiccenter.model.service.IBsUpdateNoticeService;
+import com.magic.basiccenter.model.dto.QueryNoticeDTO;
+import com.magic.basiccenter.model.dto.QueryNoticeOutDTO;
+import com.magic.basiccenter.model.entity.CuNoticeInf;
+import com.magic.basiccenter.model.mapper.CuNoticeStatusInfMapper;
+import com.magic.basiccenter.model.service.IBsNoticeStatusInfService;
 import com.magic.basiccenter.model.service.UpdateNoticeService;
-import com.magic.basiccenter.model.service.impl.BsUpdateNoticeServiceImpl;
+import com.magic.basiccenter.model.service.impl.BsNoticeStatusInfServiceImpl;
 
 @Service
 public class UpdateNoticeServiceDaoImpl implements UpdateNoticeService{
 	@Autowired
-	private IBsUpdateNoticeService bsUpdateNoticeService;
+	private IBsNoticeStatusInfService bsUpdateNoticeService;
 	@Override
-	public UpdateNoticeOutDTO selectNoticeById(UpdateNoticeDTO requestDTO) {
-		UpdateNoticeOutDTO outDTO = new UpdateNoticeOutDTO();
-		BsUpdateNoticeServiceImpl bean = SpringContextUtils.getBean(BsUpdateNoticeServiceImpl.class);
-		UpdateNoticeMapper baseMapper = bean.getBaseMapper();
-		Integer noticeId = requestDTO.getNoticeId();
-		Notice notice = baseMapper.selectById(noticeId);
-		outDTO.setNoticeName(notice.getNoticeName())
-				.setNoticeText(notice.getNoticeText())
-				.setRemindCount(notice.getRemindCount())
-				.setRemindEndTime(notice.getRemindEndTime())
-				.setRemindStartTime(notice.getRemindStartTime())
-				.setRemindStatus(notice.getRemindStatus());
-		return outDTO;
-	}
-	@Override
-	public UpdateNoticeOutDTO updateNotice(UpdateNoticeDTO requestDTO) {
-		UpdateNoticeOutDTO outDTO = new UpdateNoticeOutDTO();
-		BsUpdateNoticeServiceImpl bean = SpringContextUtils.getBean(BsUpdateNoticeServiceImpl.class);
-		UpdateNoticeMapper baseMapper = bean.getBaseMapper();
-		Notice entity = new Notice();
-		entity.setNoticeName(requestDTO.getNoticeName())
-				.setNoticeId(requestDTO.getNoticeId())
-				.setNoticeText(requestDTO.getNoticeText())
-				.setRemindCount(requestDTO.getRemindCount())
-				.setRemindEndTime(requestDTO.getRemindEndTime())
-				.setRemindStartTime(requestDTO.getRemindStartTime())
-				.setRemindStatus(requestDTO.getRemindStatus());
+	public QueryNoticeOutDTO updateNotice(QueryNoticeDTO requestDTO) {
+		QueryNoticeOutDTO outDTO = new QueryNoticeOutDTO();
+		BsNoticeStatusInfServiceImpl bean = SpringContextUtils.getBean(BsNoticeStatusInfServiceImpl.class);
+		CuNoticeStatusInfMapper baseMapper = bean.getBaseMapper();
+		CuNoticeInf entity = new CuNoticeInf();
+//		entity.setNoticeName(requestDTO.getNoticeName())
+//				.setNoticeId(requestDTO.getNoticeId())
+//				.setNoticeText(requestDTO.getNoticeText())
+//				.setRemindCount(requestDTO.getRemindCount())
+//				.setRemindEndTime(requestDTO.getRemindEndTime())
+//				.setRemindStartTime(requestDTO.getRemindStartTime())
+//				.setRemindStatus(requestDTO.getRemindStatus());
+		entity.setNiNtcName(requestDTO.getNiNtcName())
+				.setNiNtcId(requestDTO.getNiNtcId())
+				.setNiNtcText(requestDTO.getNiNtcText())
+				.setNiNtcCount(requestDTO.getNiNtcCount())
+				.setNiNtcEndTime(requestDTO.getNiNtcEndTime())
+				.setNiNtcStartTime(requestDTO.getNiNtcStartTime())
+				.setNiNtcStatus(requestDTO.getNiNtcStatus());
 		//3.2.2修改数据库中的数据
 		baseMapper.updateById(entity);
 		//4.1获取数据库中数据
-		Notice notice = baseMapper.selectById(requestDTO.getNoticeId());
-		outDTO.setNoticeName(notice.getNoticeName())
-				.setNoticeText(notice.getNoticeText())
-				.setRemindCount(notice.getRemindCount())
-				.setRemindEndTime(notice.getRemindEndTime())
-				.setRemindStartTime(notice.getRemindStartTime())
-				.setRemindStatus(notice.getRemindStatus());
+		CuNoticeInf notice = baseMapper.selectById(requestDTO.getNiNtcId());
+//		outDTO.setNoticeName(notice.getNoticeName())
+//				.setNoticeText(notice.getNoticeText())
+//				.setRemindCount(notice.getRemindCount())
+//				.setRemindEndTime(notice.getRemindEndTime())
+//				.setRemindStartTime(notice.getRemindStartTime())
+//				.setRemindStatus(notice.getRemindStatus());
+
+		outDTO.setNiNtcName(notice.getNiNtcName())
+				.setNiNtcText(notice.getNiNtcText())
+				.setNiNtcCount(notice.getNiNtcCount())
+				.setNiNtcEndTime(notice.getNiNtcEndTime())
+				.setNiNtcStartTime(notice.getNiNtcStartTime())
+				.setNiNtcStatus(notice.getNiNtcStatus());
 		return outDTO;
 	}
 }
