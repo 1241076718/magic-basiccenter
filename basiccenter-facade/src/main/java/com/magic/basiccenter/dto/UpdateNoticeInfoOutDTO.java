473f4b300cc5db9d38ed4075e4655280023f20cc
liubing <liubing1@belink.com>
2020-08-24 12:37:14 +0800

    修改模块DTO注释添加

--- basiccenter-facade/src/main/java/com/magic/basiccenter/dto/UpdateNoticeInfoOutDTO.java
+++ basiccenter-facade/src/main/java/com/magic/basiccenter/dto/UpdateNoticeInfoOutDTO.java
@@ -6,16 +6,40 @@ import java.util.Date;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+/**
+ * 
+ * @author lb
+ * @date ：Created in 2020/08/24 12:34
+ * @description：查询账户信息
+ * @modified By：
+ * @version: $
+ */
 @Data
 @Accessors(chain=true)
 public class UpdateNoticeInfoOutDTO implements Serializable{
-
-
 	private static final long serialVersionUID = -7679344017046727787L;
+	/**
+	 * 公告状态（1：已删除 2：新建 3:已发布 4：已上架 5：已下架 6：已驳回 ）
+	 */
 	private String niNtcName;
+	/**
+	 * 公告具体内容
+	 */
 	private String niNtcText;
+	/**
+	 * 提醒次数
+	 */
 	private Integer niNtcCount;
+	/**
+	 * 公告强制提醒开始时间
+	 */
 	private Date niNtcStartTime;
+	/**
+	 * 公告强制提醒结束时间
+	 */
 	private Date niNtcEndTime;
+	/**
+	 * 提醒状态（是否提醒）（0：否 1:是）
+	 */
 	private Integer niNtcRemindStatus;
 }
