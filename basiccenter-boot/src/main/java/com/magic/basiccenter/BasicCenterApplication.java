6df691a9d9c8188eee2e378d9ca4897425839ad3
zhuzh <wlvyh15820yh>
2020-08-24 14:33:54 +0800

    'mdifyapplication'

--- basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/BasicCenterApplication.java
@@ -26,7 +26,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class BasicCenterApplication {
 
     public static void main(String[] args) {
-        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "D:\\class");
         SpringApplication.run(BasicCenterApplication.class,args);
     }
 }
