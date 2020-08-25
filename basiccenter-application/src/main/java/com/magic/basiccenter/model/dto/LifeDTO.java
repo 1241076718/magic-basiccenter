9f309f896140ed188ddc75d711e7c472e89273c2
YNET <liu182273791752163.com>
2020-08-25 18:00:51 +0800

    查询调整

---
+++ basiccenter-application/src/main/java/com/magic/basiccenter/model/dto/LifeDTO.java
@@ -0,0 +1,15 @@
+package com.magic.basiccenter.model.dto;
+
+/**
+ * 数据生命状态
+ */
+public class LifeDTO {
+    /**
+     * 生存
+     */
+    public static final String SURVIVAL = "0";
+    /**
+     * 死亡
+     */
+    public static final String DEATH = "1";
+}
