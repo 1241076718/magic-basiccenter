ba41f24ee32b3073b0ca5d0d236c4adb1ec0ea78
GuanYun <yu951165089@gmail.com>
2020-08-25 17:49:08 +0800

    修改方法名

---
+++ basiccenter-infrastructure/src/main/java/com/magic/basiccenter/model/service/IBsFestivalService.java
@@ -0,0 +1,8 @@
+package com.magic.basiccenter.model.service;
+
+/**
+ * @Create by TOM on 2020/8/25 17:39
+ * @Modify by TOM on 2020/8/25 17:39
+ */
+public interface IBsFestivalService {
+}
---
+++ basiccenter-infrastructure/src/main/java/com/magic/basiccenter/model/service/impl/BsFestivalServiceImpl.java
@@ -0,0 +1,8 @@
+package com.magic.basiccenter.model.service.impl;
+
+/**
+ * @Create by TOM on 2020/8/25 17:42
+ * @Modify by TOM on 2020/8/25 17:42
+ */
+public class BsFestivalServiceImpl {
+}
