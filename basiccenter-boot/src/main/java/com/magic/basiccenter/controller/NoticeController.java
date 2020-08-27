5b4c790881a462be0a9daec26d2244a77901eff6
liubing <liubing1@belink.com>
2020-08-27 17:31:57 +0800

    测试

--- basiccenter-boot/src/main/java/com/magic/basiccenter/controller/NoticeController.java
+++ basiccenter-boot/src/main/java/com/magic/basiccenter/controller/NoticeController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 /**
  * @author ：goupc1@belink.com
  * @date ：Created in 2020/8/20 9:54
